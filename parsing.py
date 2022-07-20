8be079c74350b157afd69c628e9db9dddeae96e4
zekabobs <zekabobs@gmail.com>
2022-07-21 01:51:47 +0400

    'add_parsing'

--- parsing.py
+++ parsing.py
@@ -1,11 +1,40 @@
-from aiohttp import request
-from bs4 import BeautifulSoup
+import aiohttp
 import asyncio
+from bs4 import BeautifulSoup
 
-
-# url = https://izhlife.ru/news/?ysclid=l5kzdaa3zr146348059
+site = 'https://udm-info.ru/'
 
 
 async def parse():
-    html = request(url="https://izhlife.ru/news/?ysclid=l5kzdaa3zr146348059")
-    soup = BeautifulSoup(request(html, 'html.parser'))
+    async with aiohttp.ClientSession() as session:
+        async with session.get(site) as response:
+            print("Status:", response.status)
+            print("Content-type:", response.headers['content-type'])
+
+            if response.status == 200:
+                html = await response.text()
+                soup = BeautifulSoup(html, 'html.parser')
+
+                link = soup.find('div', class_='secondary').findChild('a')
+                section = soup.find('section', class_='cols')
+                news_img = section.findChild('img')['src']
+
+                new_link = site + link['href']
+
+                async with session.get(new_link) as new_response:
+                    new_html = await new_response.text()
+                    new_soup = BeautifulSoup(new_html, 'html.parser')
+
+                    section = new_soup.find('section', class_='cols')
+                    news_title = section.find('div', class_='cm-subtitle').text
+                    news_pargh = section.find('div', class_='paragraph').text
+                    return {'img': news_img, 'title': news_title, 'pargh': news_pargh}
+
+
+async def main():
+    task = asyncio.create_task(parse())
+    await task
+
+
+if __name__ == '__main__':
+    asyncio.get_event_loop().run_until_complete(main())
--- test.py
+++ test.py
@@ -2,18 +2,33 @@ import aiohttp
 import asyncio
 from bs4 import BeautifulSoup
 
+site = 'https://udm-info.ru/'
+
 
 async def parse():
     async with aiohttp.ClientSession() as session:
-        async with session.get('https://izhlife.ru/news/?ysclid=l5kzdaa3zr146348059') as response:
+        async with session.get(site) as response:
             print("Status:", response.status)
             print("Content-type:", response.headers['content-type'])
 
-            html = await response.text()
-            print(html)
-            soup = BeautifulSoup(html, 'html.parser')
-            ul = soup.findAll("a", attrs={'class': 'material-card__link'})
-            print(ul)
+            if response.status == 200:
+                html = await response.text()
+                soup = BeautifulSoup(html, 'html.parser')
+
+                link = soup.find('div', class_='secondary').findChild('a')
+                section = soup.find('section', class_='cols')
+                news_img = section.findChild('img')['src']
+
+                new_link = site + link['href']
+
+                async with session.get(new_link) as new_response:
+                    new_html = await new_response.text()
+                    new_soup = BeautifulSoup(new_html, 'html.parser')
+
+                    section = new_soup.find('section', class_='cols')
+                    news_title = section.find('div', class_='cm-subtitle').text
+                    news_pargh = section.find('div', class_='paragraph').text
+                    return {'img': news_img, 'title': news_title, 'pargh': news_pargh}
 
 
 async def main():
